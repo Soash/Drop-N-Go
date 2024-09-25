23409ad53ba8d4e64e54930cdc1063390d3d122f
soash <soash2002@gmail.com>
2024-09-25 11:50:10 +0600

    commit

--- build.sh
+++ build.sh
@@ -3,12 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-
-pip install pillow requests django-tinymce psycopg2-binary
-
-
 python manage.py collectstatic --no-input
-
+python manage.py makemigrations
 python manage.py migrate
-
 
